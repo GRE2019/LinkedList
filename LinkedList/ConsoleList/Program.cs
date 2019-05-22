df44350de0ce41c198ba17ff503e17382dbf5ba0
Cesar Lorenzo <clorenzo@grtle.com>
2019-05-22 16:53:36 -0500

    Insert

---
+++ LinkedList/ConsoleList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace ConsoleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Añade al inicio:");
+            Node list = new Node();
+            LinkedListOperation linkedListOperation = new LinkedListOperation();
+
+            list = linkedListOperation.createStart("Hola", list, null);
+            list = linkedListOperation.createStart("Querido", list, null);
+            list = linkedListOperation.createStart("Mundo", list, null);
+
+
+            linkedListOperation.printList(list);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinkedList/LinkedList/LinkedListOperation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListOperation
+    {
+        public Node createStart(Object item, Node start, Node end)
+        {
+            Node node = new Node();
+
+            node.item = item;
+            node.next = start;
+            start = node;
+
+            return node;
+        }
+
+        public Node CreateEnd(Object item, Node list)
+        {
+            if (list != null)
+            {
+                list = new Node();
+
+                list.item = item;
+                list.next = null;
+
+                return list;
+            }
+            else
+            {
+                Node start = new Node();
+
+                start.item = item;
+
+                Node current = list;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = list;
+
+                return current;
+            }
+        }
+
+        public void printList(Node list)
+        {
+            Node node = new Node();
+
+            node = list;
+
+            while (node != null)
+            {
+                Console.WriteLine(node.item);
+
+                if (node.next.next != null)
+                {
+                    node = node.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
